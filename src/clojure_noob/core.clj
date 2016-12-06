175a729725915dd02cb5443c29114fded808efa6
Emily Platzer <emilyplatzer@gmail.com>
2016-12-05 18:40:29 -0700

    Added solutions for exercises 1-3 from chapter 3

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,28 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot"))
+
+(println (str "Hello my name is " "Emily"))
+(println (vector 1 2 3 5 8 13))
+(println (list 1 2 3 5 8 13))
+(println (hash-map :elowyn 9 :luke 9 :emily 12))
+(println (hash-set :elowyn :luke :emily))
+(println (hash-set :elowyn :luke :emily :emily)) ;dups disappear
+
+(defn plus100
+  [number]
+  (+ number 100))
+(println (plus100 1))
+(println (plus100 100))
+(println (plus100 1000))
+
+(defn dec-maker
+  [decrementor]
+  (fn [number]
+    (- number decrementor)))
+(def dec9 (dec-maker 9))
+(println (dec9 10))
+(println (dec9 9))
+(println (dec9 90))
+(def dec42 (dec-maker 42))
+(println (dec42 43))
